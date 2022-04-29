f415d98cd79a003ed03d7f7cff5aa579b2e3b9b6
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2022-04-29 19:44:45 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Tweet } from "./componentes/Tweet";
+
+function App() {
+  return (
+    <div>
+      <Tweet />
+      <Tweet />
+      <Tweet />
+    </div>
+  );
+}
+
+export default App;
