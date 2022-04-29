47e8db28fafb163906636d3adb72d2cd765d7f20
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2022-04-29 20:09:03 -0300

    Adicionando estado no componente

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
+import { useState } from "react";
 import { Tweet } from "./componentes/Tweet";
 
 function App() {
+  const [tweets, setTweets] = useState<string[]>([
+    "Tweet 1",
+    "Tweet 2",
+    "Tweet 3",
+    "Tweet 4",
+  ]);
+  function createTweet() {
+    setTweets([...tweets, "Tweet" + tweets.length]);
+  }
+
   return (
     <div>
-      <Tweet text="Tweet 1" />
-      <Tweet text="Tweet 2" />
-      <Tweet text="Tweet 3" />
+      {tweets.map((tweet) => {
+        return <Tweet text={tweet} />;
+      })}
+
+      <button onClick={createTweet}>Adicionar Tweet</button>
     </div>
   );
 }
